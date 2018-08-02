c0440bdc75b9d20273ab42e4ab4afd976c43e708
BiggaHD <dzenis.hankusic@gmail.com>
2018-08-02 12:59:04 +0200

    Fixed Delete button

--- app.js
+++ app.js
@@ -132,10 +132,7 @@ app.get('/files/:filename', (req, res) => {
 // @desc  Delete file
 app.delete('/files/:id', (req, res) => {
   gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
-    if (err) {
-      return res.status(404).json({ err: err });
-    }
-
+    if (err) res.status(404).json({ err: err });
     res.redirect('/');
   });
 });
