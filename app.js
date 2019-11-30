b0a52dc6526280a7baf2de00b3a0f780294afa04
BiggaHD <dzenis.hankusic@gmail.com>
2019-11-30 05:25:05 +0100

    v 2.0.0

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
-const methodOverride = require('method-override');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
+const mongoURI = require("./config/keys");
 
 const app = express();
-app.use("/public", express.static('public')); 
+app.use("/public", express.static("public"));
 
 // Middleware
 app.use(bodyParser.json());
-app.use(methodOverride('_method'));
-app.set('view engine', 'ejs');
-
-// Mongo URI
-const mongoURI = 'mongodb://BiggaHD:Test123@ds131721.mlab.com:31721/file_upload';
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
 
 // Create mongo connection
-const conn = mongoose.createConnection(mongoURI);
+const conn = mongoose.createConnection(mongoURI, { useNewUrlParser: true });
 
 // Init gfs
 let gfs;
 
-conn.once('open', () => {
+conn.once("open", () => {
   // Init stream
   gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
+  gfs.collection("uploads");
 });
 
 // Create storage engine
@@ -34,51 +32,59 @@ const storage = new GridFsStorage({
   url: mongoURI,
   file: (req, file) => {
     return new Promise((resolve, reject) => {
-        const fileInfo = {
-          filename: file.originalname,
-          bucketName: 'uploads'
-        };
-        resolve(fileInfo);
-      });
-    }
+      const fileInfo = {
+        filename: file.originalname,
+        bucketName: "uploads"
+      };
+      resolve(fileInfo);
+    });
+  }
 });
 
 const upload = multer({ storage });
 
 // @route GET /
 // @desc Loads form
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   gfs.files.find().toArray((err, files) => {
     // Check if files
     if (!files || files.length === 0) {
-      res.render('index', { files: false });
+      res.render("index", { files: false });
     } else {
       files.map(file => {
-        file.contentType === 'image/jpeg' || file.contentType === 'image/png' ? file.isImage = true : file.isImage = false;
-        file.contentType === 'audio/mpeg' || file.contentType === 'audio/mp3' ? file.isAudio = true : file.isAudio = false;
-        file.contentType === 'video/mp4'  || file.contentType === 'video/avi' ? file.isVideo = true : file.isVideo = false;
-        file.contentType === 'text/plain' ? file.isText = true : file.isText = false;
+        file.contentType === "image/jpeg" || file.contentType === "image/png"
+          ? (file.isImage = true)
+          : (file.isImage = false);
+        file.contentType === "audio/mpeg" || file.contentType === "audio/mp3"
+          ? (file.isAudio = true)
+          : (file.isAudio = false);
+        file.contentType === "video/mp4" || file.contentType === "video/avi"
+          ? (file.isVideo = true)
+          : (file.isVideo = false);
+        file.contentType === "text/plain"
+          ? (file.isText = true)
+          : (file.isText = false);
       });
-      res.render('index', { files });
+      res.render("index", { files });
     }
   });
 });
 
 // @route POST /upload
 // @desc  Uploads file to DB
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
   // res.json({ file: req.file });
-  res.redirect('/');
+  res.redirect("/");
 });
 
 // @route GET /files
 // @desc  Display all files in JSON
-app.get('/files', (req, res) => {
+app.get("/files", (req, res) => {
   gfs.files.find().toArray((err, files) => {
     // Check if files
     if (!files || files.length === 0) {
       return res.status(404).json({
-        err: 'No files exist'
+        err: "No files exist"
       });
     }
     // Files exist
@@ -87,26 +93,31 @@ app.get('/files', (req, res) => {
 });
 
 // @ Check if the file is a valid IMAGE || AUDIO || VIDEO || TEXT file format
-app.get('/file/:filename', (req, res) => {
+app.get("/file/:filename", (req, res) => {
   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
     // Check if file
     if (!file || file.length === 0) {
       return res.status(404).json({
-        err: 'No file exists'
+        err: "No file exists"
       });
     }
 
     // Check the file types
-    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png' ||
-        file.contentType === 'audio/mpeg' || file.contentType === 'audio/mp3' || 
-        file.contentType === 'video/mp4'  || file.contentType === 'video/avi' ||
-        file.contentType === 'text/plain') {
+    if (
+      file.contentType === "image/jpeg" ||
+      file.contentType === "image/png" ||
+      file.contentType === "audio/mpeg" ||
+      file.contentType === "audio/mp3" ||
+      file.contentType === "video/mp4" ||
+      file.contentType === "video/avi" ||
+      file.contentType === "text/plain"
+    ) {
       // Read output to browser
       const readstream = gfs.createReadStream(file.filename);
       readstream.pipe(res);
     } else {
       res.status(404).json({
-        err: 'Not an a valid file format'
+        err: "Not an a valid file format"
       });
     }
   });
@@ -114,12 +125,12 @@ app.get('/file/:filename', (req, res) => {
 
 // @route GET /files/:filename
 // @desc  Display/ Download single file object ... other than the usual ones
-app.get('/files/:filename', (req, res) => {
+app.get("/files/:filename", (req, res) => {
   gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
     // Check if file
     if (!file || file.length === 0) {
       return res.status(404).json({
-        err: 'No file exists'
+        err: "No file exists"
       });
     } else {
       const readstream = gfs.createReadStream(file.filename);
@@ -130,10 +141,10 @@ app.get('/files/:filename', (req, res) => {
 
 // @route DELETE /files/:id
 // @desc  Delete file
-app.delete('/files/:id', (req, res) => {
-  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+app.delete("/files/:id", (req, res) => {
+  gfs.remove({ _id: req.params.id, root: "uploads" }, (err, gridStore) => {
     if (err) res.status(404).json({ err: err });
-    res.redirect('/');
+    res.redirect("/");
   });
 });
 
